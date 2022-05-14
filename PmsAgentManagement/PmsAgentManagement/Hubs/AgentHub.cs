21b319eab88710840e6d5e3237074bd85337ca3e
Александр Пирогов <a.pirogov@citrus-soft.ru>
2022-05-14 23:48:42 +0300

    Регистрация пользователя

--- PmsAgentManagement/PmsAgentManagement/Hubs/AgentHub.cs
+++ PmsAgentManagement/PmsAgentManagement/Hubs/AgentHub.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Infrastructure;
+using Microsoft.AspNet.SignalR.Transports;
 using PmsAgentManagement.HttpApi;
 
 namespace PmsAgentManagement.Hubs
@@ -10,20 +14,48 @@ namespace PmsAgentManagement.Hubs
     {
         private readonly IHttpApi _api;
 
+        private static Dictionary<string, ITrackingConnection> _userConnections = new Dictionary<string, ITrackingConnection>();
+
         public AgentHub()
         {
             _api = new HttpNpbApi(); //Не работает DI через сервис контейнер
+           // _userConnections = new Dictionary<string, Connection>();
         }
         
-        public string Request(string request)
+        public Task Request(string guid, string request)
         {
-            Console.WriteLine(request);
-            return _api.GetData();
+            var connection = _userConnections[guid];
+
+            if (!connection.IsAlive)
+            {
+                throw new Exception("Connection is died");
+            }
+            
+            var response = _api.GetData();
+            return Clients.User(connection.ConnectionId).AddMessage(response);
         }
 
-        public void Register(long userId)
+        public bool Register(string guid)
         {
-            Groups.Add(Context.ConnectionId, userId.ToString(CultureInfo.InvariantCulture));
+            var heartBeat = GlobalHost.DependencyResolver.Resolve<ITransportHeartbeat>();
+            var connectionId = Context.ConnectionId;
+            var connection = heartBeat.GetConnections().FirstOrDefault(c => c.ConnectionId == connectionId);
+
+            try
+            {
+                if (connection != null && connection.IsAlive)
+                {
+                    _userConnections.Remove(guid);
+                    _userConnections.Add(guid, connection);
+                    return true;
+                }
+
+                throw new Exception("Connection is died");
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
         }
 
         public void ToGroup(long id, string message)
